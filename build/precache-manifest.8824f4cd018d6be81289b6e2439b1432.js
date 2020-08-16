f0be04c841d83c76a39ed4a8aa8eff0747173740
Hassan <hassan8b2000@gmail.com>
2020-08-15 22:44:14 -0400

    initial commit

---
+++ build/precache-manifest.8824f4cd018d6be81289b6e2439b1432.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9cd0b2970742f37720234f6a971d209",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b8bdac609d6aca34969",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "43c9f8447e1fb51ab67d",
+    "url": "/static/css/main.81bb362c.chunk.css"
+  },
+  {
+    "revision": "9b8bdac609d6aca34969",
+    "url": "/static/js/2.cdc7a50b.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.cdc7a50b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43c9f8447e1fb51ab67d",
+    "url": "/static/js/main.35daa3cb.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
